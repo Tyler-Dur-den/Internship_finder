c3d4289c550cd0371476c9bd9d2b458482c8aa71
Tyler-Dur-den <attherate798@gmail.com>
2026-08-18 15:45:55 +0530

    add internship search, metadata extraction and graph

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from langchain_groq import ChatGroq
 from typing import TypedDict, Annotated, Literal, List
 from langgraph.prebuilt import ToolNode, tools_condition
 from pydantic import BaseModel, Field
-from tavily import TavilyClient
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_core.tools import tool
 import os
+
+from search_tool import tavily_search
 
 llm = ChatGroq(model_name="llama-3.3-70b-versatile")
 
@@ -25,11 +26,41 @@ class Internship_metadata(BaseModel):
 class InternshipState(TypedDict):
     messages: Annotated[List[BaseMessage], add_messages]
     userskills : list[str]
-    location : str
-    interships_found : list[dict]
+    internships_found : list[dict]
     skill_gap : list[str]
     website_link : list[str]
 
-@tool
-def search_internships():
+def internship_finder(state:InternshipState):
+    query = f"AI engineer internship {' '.join(state['userskills'])} Bangalore 2026 site:internshala.com OR site:wellfound.com OR site:linkedin.com"
+    internships = tavily_search(query)
+    return {
+        "internships_found" : internships,
+        "website_link" : internships["url"]
+    }
+
+extraction_llm = llm.with_structured_output(Internship_metadata)
+
+def extraction(state:InternshipState):
+    raw_results = state["internships_found"]
+
+    prompt = f"""Extract internship details from the following search results
+    results : {raw_results}
+    """
+
+    internship_list_container = extraction_llm.invoke(prompt)
+    listings_dict = [item.model_dump() for item in internship_list_container.listings]
     
+    return {
+        "internships_found": listings_dict
+    }
+
+builder = StateGraph(InternshipState)
+
+builder.add_node("internship_finder",internship_finder)
+builder.add_node("extraction",extraction)
+
+builder.add_edge(START, "internship_finder")
+builder.add_edge("internship_finder","extraction")
+builder.add_edge("extraction", END)
+
+graph = builder.compile()
---
+++ search_tool.py
@@ -0,0 +1,18 @@
+from tavily import TavilyClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = TavilyClient(
+    api_key=os.getenv("TAVILY_API_KEY")
+)
+
+def tavily_search(query):
+    response = client.search(
+        query = query,
+        max_results=15,
+        search_depth="advanced"
+    )
+    
+    return response.get("results", [])
