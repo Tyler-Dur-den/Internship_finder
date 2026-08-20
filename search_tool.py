00f6ab402f5750c3e7af70e0b2966ee6cb9e6a30
Tyler-Dur-den <attherate798@gmail.com>
2026-08-20 18:11:26 +0530

    feat: add Streamlit UI and parallelize resume & search nodes

--- search_tool.py
+++ search_tool.py
@@ -1,13 +1,14 @@
-from serpapi import GoogleSearch
 import os
 from dotenv import load_dotenv
+from serpapi import GoogleSearch
+
 load_dotenv()
 
-def serpapi_search():
+def serpapi_search(domain: str, location: str = "Bangalore"):
     params = {
         "engine": "google_jobs",
-        "q":"Ai engineer intern",
-        "location": "Bangalore",
+        "q": f"{domain} intern",
+        "location": location,
         "api_key": os.getenv("SERPAPI_API_KEY")
     }
     search = GoogleSearch(params)
